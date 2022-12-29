4ed6215a2112ebb1a8ae4503bb61b3cb8a41fb96
rocknrollbassplayer <121578167+rocknrollbassplayer@users.noreply.github.com>
2022-12-29 09:43:39 -0500

    Update filename.sh

--- filename.sh
+++ filename.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#from command line $ ./filename.sh <enter name of file you are searching for>
+#This script will tell you if the file exists or not
 
 FILENAME=$1
 if [ -f "$FILENAME" ]; then
